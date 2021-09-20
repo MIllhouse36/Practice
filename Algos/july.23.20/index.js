9f9738557d260c12bafb9604a08f702937b51849
Kevin <millhousekevin@gmail.com>
2021-09-20 11:23:22 -0400

    cleaning

--- Algos/july.23.20/index.js
+++
@@ -1,9 +0,0 @@
-function negatizer(numString){
-  // var negNUm = numString * -1;
-//  if(isNaN(negNUm)){
-//   throw `input must be coercible to a number`
-//  }
- return console.log(typeof numString);
-}
-
-negatizer(["cat"]); 
--- Algos/randomstuff/index.js
+++
@@ -1,9 +0,0 @@
-
-var random_value = stuff[ Math.floor( Math.random() * 14 ) ];
-  var stuff = [10,34,56,67,93,120,137,168,259,280,311,342,413,514];
-
-for (var i = 0; i < stuff.length; i += 1) {
- console.log(stuff[i]);
-}
-
-console.log(random_value)
