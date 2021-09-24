1b2e5fc163b9343407b8b6492bc42feb20ac3891
Kevin <millhousekevin@gmail.com>
2021-09-24 12:56:31 -0400

    intuit tut

---
+++ intuit_tut/src/index.js
@@ -0,0 +1,15 @@
+function openNum(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabContent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+openNum();
