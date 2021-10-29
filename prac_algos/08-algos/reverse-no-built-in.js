6eccf66a083634863a0c19b337ebe74b75338d50
Kevin <millhousekevin@gmail.com>
2021-10-28 20:31:41 -0400

    cleaning up my jawn

--- prac_algos/08-algos/reverse-no-built-in.js
+++ prac_algos/08-algos/reverse-no-built-in.js
@@ -2,11 +2,7 @@ const reverse = (str)=>{
   let words = str.split("").reverse();
   let results = "" 
   for(let i=0; i<words.length; i++){
-    // let letters = words[i].split('').reverse().join("");
     results += words[i].split("").reverse().join("");
-
-    // console.log(letters)
-    
   }
   return results
 }
