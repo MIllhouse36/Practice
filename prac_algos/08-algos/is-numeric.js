be9e4507940d0a757f33fa19f346a62f456c612a
Kevin <millhousekevin@gmail.com>
2021-11-02 16:31:42 -0400

    is numeric started

--- prac_algos/08-algos/is-numeric.js
+++ prac_algos/08-algos/is-numeric.js
@@ -1,9 +1,59 @@
-const isNumeric = (str)=>{
-  for(let i=0; i<str.length; i++){
-    let char = str[i]
-    
+// const isNumeric = (str)=>{
+//   for(let i=0; i<str.length; i++){
+//     let char = str[i]
+
+//   }
+// }
+
+var isNumeric = function (str) {
+  var digits = {
+    0: true,
+    1: true,
+    2: true,
+    3: true,
+    4: true,
+    5: true,
+    6: true,
+    7: true,
+    8: true,
+    9: true,
+  };
+
+  var hasNum = false;
+  var hasDecimal = false;
+
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i];
+
+    if (digits[char]) {
+      hasNum = true;
+      continue;
+    }
+
+    if (i === 0) {
+      if (char === "-" || char === "+") {
+        continue;
+      }
+    }
+
+    if (char === ".") {
+      if (hasDecimal === true) {
+        return false;
+      }
+
+      hasDecimal = true;
+      continue;
+    }
+
+    return false;
   }
-}
-console.log(Number())
-isNumeric("-0123.00");
-isNumeric("+-a13");
+
+  if (hasNum) {
+    return true;
+  }
+
+  return false;
+};
+
+console.log(isNumeric("0123.00"));
+console.log(isNumeric("+-13"));
